42c99aeaeb34b183f3eef991c8e099e16adee1bf
Gouri <goury.oneteam@gmail.com>
2025-02-20 15:11:27 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+const express=require('express')
+const connectDB = require('./src/config/db');
+
+const cors = require("cors");
+
+connectDB();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use("/uploads", express.static("uploads"));
+
+
+app.use("/api/auth", require("./src/routes/authRoutes"));  // Authentication Routes
+app.use("/api/admin", require("./src/routes/adminroute"));  // Admin Routes
+app.use("/api/student", require("./src/routes/studentroute"));  // student Routes
+app.use("/api/admin", require("./src/routes/batchallocationroutes"));//admin
+app.use("/api/trainer", require("./src/routes/trainerroutes"));//admin
+
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
